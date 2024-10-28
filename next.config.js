2f0cf3a8fe3f15948d6b05405ceef7d6e7de6d67
Shriniket Acharya <shriniketacharya@gmail.com>
2024-10-28 14:33:38 +0530

    Fixed postcss config issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
